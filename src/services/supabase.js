19cb606e06a06fbe53a4c403c8e21c70cd72bc08
Ahmed <eilafsama@Ahmeds-MacBook-Pro-2.local>
2023-08-03 14:03:01 +0300

    Added documentation to the cade

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,16 @@
+/*
+
+Initalizing supabase connection by importing createClient from the supabase npm package.
+
+*/
 import { createClient } from "@supabase/supabase-js";
+
+//Supabase Credentials to access the database
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+//Initializing the client with the database
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+//Exporting the client for use in the application components
 export default supabase;
