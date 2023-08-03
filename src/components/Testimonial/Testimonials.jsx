fb9e9419903bbdffb50dde858a7c48fbe99b7761
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-04 00:20:01 +0530

    Add testimonial's components!

---
+++ src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from "uuid";
+import classes from "./Testimonials.module.css";
+import UserImage from "../../assets/user.png";
+import TestimonialItem from "./TestimonialItem";
+
+const TESTIMONIALS = [
+  {
+    profileImage: UserImage,
+    name: "Imran Biswas",
+    position: "Fullstack Development",
+    testimonial:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error nihil architecto iure accusamus non commodi eveniet, illum repudiandae odio?",
+  },
+  {
+    profileImage: UserImage,
+    name: "Imran Biswas",
+    position: "Fullstack Development",
+    testimonial:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error nihil architecto iure accusamus non commodi eveniet, illum repudiandae odio?",
+  },
+  {
+    profileImage: UserImage,
+    name: "Imran Biswas",
+    position: "Fullstack Development",
+    testimonial:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error nihil architecto iure accusamus non commodi eveniet, illum repudiandae odio?",
+  },
+  {
+    profileImage: UserImage,
+    name: "Imran Biswas",
+    position: "Fullstack Development",
+    testimonial:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error nihil architecto iure accusamus non commodi eveniet, illum repudiandae odio?",
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section className={`flex-col ${classes.testimonials}`}>
+      {TESTIMONIALS.map((testimonial) => (
+        <TestimonialItem
+          key={uuidv4()}
+          testimonialDetails={{ ...testimonial }}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Testimonials;
