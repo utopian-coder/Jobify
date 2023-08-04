ff11ab2e2364a1d14acb8e2797085addf6324e91
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-04 16:28:32 +0530

    Refactored testimonials section!

--- src/components/Testimonial/Testimonials.jsx
+++ src/components/Testimonial/Testimonials.jsx
@@ -25,24 +25,20 @@ const TESTIMONIALS = [
     testimonial:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error nihil architecto iure accusamus non commodi eveniet, illum repudiandae odio?",
   },
-  {
-    profileImage: UserImage,
-    name: "Imran Biswas",
-    position: "Fullstack Development",
-    testimonial:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error nihil architecto iure accusamus non commodi eveniet, illum repudiandae odio?",
-  },
 ];
 
 const Testimonials = () => {
   return (
-    <section className={`flex-col ${classes.testimonials}`}>
-      {TESTIMONIALS.map((testimonial) => (
-        <TestimonialItem
-          key={uuidv4()}
-          testimonialDetails={{ ...testimonial }}
-        />
-      ))}
+    <section>
+      <h2>What Our Customers Say</h2>
+      <div className={`flex-col ${classes.testimonials}`}>
+        {TESTIMONIALS.map((testimonial) => (
+          <TestimonialItem
+            key={uuidv4()}
+            testimonialDetails={{ ...testimonial }}
+          />
+        ))}
+      </div>
     </section>
   );
 };
