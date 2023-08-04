0404e9f3460bde5c2a3352e3835dbbb142416749
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-04 23:06:02 +0530

    Add necessary components.

---
+++ src/components/PostJobForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostJobForm = () => {
+  return (
+    <article>
+      <h2>Post Job</h2>
+      <form></form>
+    </article>
+  );
+};
+
+export default PostJobForm;
