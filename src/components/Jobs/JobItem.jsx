4ad766a78c525a371e6c5f95ba1b5a22ec1f08ea
IbadRehman360 <ibadhashim4@gmail.com>
2023-08-05 17:27:49 +0500

    feat: Add logic and update List component styling
    - Added new logic to handle XYZ functionality.
    - Updated the List component to display items in a more user-friendly way.
    - Improved the styling of the List component for better visual appeal.

--- src/components/Jobs/JobItem.jsx
+++ src/components/Jobs/JobItem.jsx
@@ -1,19 +1,63 @@
+import "./Jobitem.css";
+
+import { useState, useEffect } from "react";
+
+const getRelativeDate = (datePosted) => {
+  const now = new Date();
+  const difference = now - datePosted;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneMonth = 30 * oneDay;
+  const oneYear = 365 * oneDay;
+
+  if (difference < oneDay) {
+    return "Today";
+  } else if (difference < oneMonth) {
+    const daysAgo = Math.floor(difference / oneDay);
+    return `${daysAgo} day${daysAgo === 1 ? "" : "s"} ago`;
+  } else if (difference < oneYear) {
+    const monthsAgo = Math.floor(difference / oneMonth);
+    return `${monthsAgo} month${monthsAgo === 1 ? "" : "s"} ago`;
+  } else {
+    const yearsAgo = Math.floor(difference / oneYear);
+    return `${yearsAgo} year${yearsAgo === 1 ? "" : "s"} ago`;
+  }
+};
+
 const JobItem = ({ jobDetails }) => {
-  const { logo, name, time, description, title, location, pay } = jobDetails;
+  const [relativeDate, setRelativeDate] = useState("");
+
+  useEffect(() => {
+    const datePosted = new Date(jobDetails.datePosted);
+    const relativeDateStr = getRelativeDate(datePosted);
+    setRelativeDate(relativeDateStr);
+  }, [jobDetails.datePosted]);
+
   return (
-    <article>
-      <div>
-        <img src={logo} alt='company logo' />
-        <span>{name}</span>
+    <article className="job-item">
+      <div className="company-info">
+        <div className="company-logo-name">
+          <img
+            className="company-logo"
+            src={jobDetails.logo}
+            alt="company logo"
+          />
+          <span className="company-name">{jobDetails.name}</span>
+        </div>
+        <p className="job-date">🕑 {relativeDate}</p>{" "}
       </div>
-      <details>
-        <summary>Job Description</summary>
-        <p>{description}</p>
+
+      <details className="job-details">
+        <summary className="job-description-header">Job Description</summary>
+        <p className="job-description-text">{jobDetails.description}</p>
       </details>
-      <div>
-        <div>{title}</div>
-        <div>{location}</div>
-        <div>{pay} per month</div>
+      <hr />
+      <div className="job-info">
+        <div className="job-title">{jobDetails.title}</div>
+
+        <div className="job-location">📍 {jobDetails.location}</div>
+        <div className="job-pay">💲{jobDetails.pay} month</div>
+        <div className="job-type">⌛ {jobDetails.type}</div>
       </div>
     </article>
   );
--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,12 +1,76 @@
-import React from "react";
+import { useState } from "react";
 import JobSearchOptions from "./JobSearchOptions";
+import JobItem from "./JobItem";
 
-const Jobs = () => {
+const Jobs = ({ demoJobs }) => {
+  const [selectedDate, setSelectedDate] = useState("any-date");
+  const [selectedCountry, setSelectedCountry] = useState("any-country");
+  const [jobType, setJobType] = useState("any-time");
+  const [searchInput, setSearchInput] = useState("");
+  const filterJobsByDate = (jobs) => {
+    const currentDate = new Date();
+
+    switch (selectedDate) {
+      case "any-date":
+        return jobs;
+      case "past-month":
+        return jobs.filter(
+          (job) =>
+            job.datePosted >= new Date(currentDate - 30 * 24 * 60 * 60 * 1000)
+        );
+      case "past-week":
+        return jobs.filter(
+          (job) =>
+            job.datePosted >= new Date(currentDate - 7 * 24 * 60 * 60 * 1000)
+        );
+      case "past-24-hours":
+        return jobs.filter(
+          (job) => job.datePosted >= new Date(currentDate - 24 * 60 * 60 * 1000)
+        );
+      default:
+        return jobs;
+    }
+  };
+  const filterJobsByTitle = (jobs) => {
+    if (!searchInput || searchInput.trim() === "") {
+      return jobs;
+    } else {
+      const searchTerm = searchInput.toLowerCase().trim();
+      return jobs.filter((job) => job.title.toLowerCase().includes(searchTerm));
+    }
+  };
+  const filterJobsByCountry = (jobs) => {
+    if (selectedCountry === "any-country") {
+      return jobs;
+    } else {
+      return jobs.filter((job) => job.location === selectedCountry);
+    }
+  };
+
+  const filterJobsByType = (jobs) => {
+    if (jobType === "any-time") {
+      return jobs;
+    } else {
+      return jobs.filter((job) => job.type === jobType);
+    }
+  };
+  const filteredByDate = filterJobsByDate(demoJobs);
+  const filteredByCountry = filterJobsByCountry(filteredByDate);
+  const filteredByJobTitle = filterJobsByTitle(filteredByCountry);
+  const filteredAndSorted = filterJobsByType(filteredByJobTitle);
+
   return (
     <section>
       <h2>Recent Jobs</h2>
-      <JobSearchOptions />
-      {/* TODO:  render job items*/}
+      <JobSearchOptions
+        onSelectDate={setSelectedDate}
+        onSelectCountry={setSelectedCountry}
+        onJobType={setJobType}
+        onsetSearchInput={setSearchInput}
+      />
+      {filteredAndSorted.map((job) => (
+        <JobItem key={job.id} jobDetails={job} />
+      ))}
     </section>
   );
 };
