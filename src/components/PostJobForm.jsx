15ab7bbda57e99fd66d83f980cf32c0fd8ec5428
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-10 11:10:58 +0530

    Fix form design!

--- src/components/PostJobForm.jsx
+++ src/components/PostJobForm.jsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./PostJobForm.css";
 import createJob from "../hooks/createJob";
 import { useState } from "react";
 
@@ -57,149 +58,139 @@ const PostJobForm = () => {
   };
 
   return (
-    <div className="new-div" style={{ with: "100%" }}>
-      <h1
-        style={{
-          fontSize: "3rem",
-          fontWeight: "bold",
-          color: "#486284",
-          marginBottom: "3rem",
-          marginTop: "8rem",
-          textAlign: "center",
-        }}
-      >
-        Job Posting 💼
-      </h1>
+    <article className='container form-container'>
+      <h1>Job Posting 💼</h1>
       <br />
-      <div className="col-md-8 offset-md-2  ">
+      <div className='col-md-8 offset-md-2  form'>
         <form
           onSubmit={handleSubmit}
-          method="POST"
+          method='POST'
           noValidate
-          encType="multipart/form-data"
+          encType='multipart/form-data'
         >
-          <div className="mb-3">
-            <label className="form-label" htmlFor="name">
+          <div className='mb-3'>
+            <label className='form-label' htmlFor='name'>
               Company Name
             </label>
             <input
-              className="form-control"
-              type="text"
-              id="name"
+              className='form-control'
+              type='text'
+              id='name'
               required
               onChange={handleChange}
               value={formData.name}
             />
-            <div className="valid-feedback">Looks good!</div>
+            <div className='valid-feedback'>Looks good!</div>
           </div>
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="location">
+          <div className='mb-3'>
+            <label className='form-label' htmlFor='location'>
               Company Location
             </label>
             <input
-              className="form-control"
-              type="text"
-              id="location"
+              className='form-control'
+              type='text'
+              id='location'
               required
               onChange={handleChange}
               value={formData.location}
             />
-            <div className="valid-feedback">Looks good!</div>
+            <div className='valid-feedback'>Looks good!</div>
           </div>
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="positionTitle">
+          <div className='mb-3'>
+            <label className='form-label' htmlFor='positionTitle'>
               Job Position
             </label>
             <input
-              className="form-control"
-              type="text"
-              id="positionTitle"
+              className='form-control'
+              type='text'
+              id='positionTitle'
               required
               onChange={handleChange}
               value={formData.positionTitle}
             />
-            <div className="valid-feedback">Looks good!</div>
+            <div className='valid-feedback'>Looks good!</div>
           </div>
 
-          <div className="mb-3">
-            <label className="form-label" htmlFor="pay">
+          <div className='mb-3'>
+            <label className='form-label' htmlFor='pay'>
               Salary
             </label>
-            <div className="input-group">
-              <span className="input-group-text" id="price-label">
+            <div className='input-group'>
+              <span className='input-group-text' id='price-label'>
                 $
               </span>
               <input
-                type="text"
-                className="form-control"
-                id="pay"
-                placeholder="0.00"
-                aria-label="price"
+                type='text'
+                className='form-control'
+                id='pay'
+                placeholder='0.00'
+                aria-label='price'
                 required
                 onChange={handleChange}
                 value={formData.pay}
               />
             </div>
-            <div className="valid-feedback">Looks good!</div>
+            <div className='valid-feedback'>Looks good!</div>
           </div>
 
-          <div className="mb-3 mt-3">
-            <label className="form-label" htmlFor="description">
+          <div className='mb-3 mt-3'>
+            <label className='form-label' htmlFor='description'>
               Job Description
             </label>
             <textarea
-              className="form-control"
-              type="text"
-              id="description"
-              name="campground[description]"
+              className='form-control'
+              type='text'
+              id='description'
+              name='campground[description]'
               required
               onChange={handleChange}
               value={formData.description}
             ></textarea>
-            <div className="valid-feedback">Looks good!</div>
+            <div className='valid-feedback'>Looks good!</div>
           </div>
 
-          <div style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-            <label htmlFor="type">Select an Option:</label>
+          {/* <div style={{ marginTop: "2rem", marginBottom: "2rem" }}> */}
+          <div>
+            <label htmlFor='type'>Select an Option:</label>
             <select
-              className="form-control"
-              id="type"
+              className='form-control'
+              id='type'
               onChange={handleSelect}
               value={formData.type}
             >
-              <option value="">Select one...</option>
-              <option value="Full-time">Full-time</option>
-              <option value="Part-time">Part-time</option>
+              <option value=''>Select one...</option>
+              <option value='Full-time'>Full-time</option>
+              <option value='Part-time'>Part-time</option>
             </select>
           </div>
-          <div className="form-file custom-file">
+          <div className='form-file custom-file'>
             <input
-              style={{ width: "390px" }}
-              type="file"
-              className="form-file-input"
-              id="logo"
-              name="logo"
+              style={{ width: "390px", marginBlock: "0.5rem" }}
+              type='file'
+              className='form-file-input'
+              id='logo'
+              name='logo'
               value={formData.logo}
               onChange={handleChange}
             />
-            <label className="form-file-label" htmlFor="image">
-              <span className="form-file-text custom-file-label">
+            <label className='form-file-label' htmlFor='image'>
+              <span className='form-file-text custom-file-label'>
                 Choose image(s)...
               </span>
-              <span className="form-file-button">Browse</span>
+              <span className='form-file-button'>Browse</span>
             </label>
           </div>
 
-          <div className="mb-3 mt-4 ">
-            <button className="btn custom-button" type="submit">
+          <div className='mb-3 mt-4 '>
+            <button className='btn custom-button' type='submit'>
               Submit Job
             </button>
           </div>
         </form>
       </div>
-    </div>
+    </article>
   );
 };
 
